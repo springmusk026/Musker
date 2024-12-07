11671f64b37370b61a1ab8f20eaa8db9442e1c6f
Musk Modder <springmusk@gmail.com>
2024-12-07 14:11:52 +0545

    added homepage

--- src/ChatInterface.tsx
+++ src/ChatInterface.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MessageList } from './components/MessageList';
 import { ImagePopup } from './components/ImagePopup';
